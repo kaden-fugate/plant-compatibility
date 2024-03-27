b34266ae253369d3a31dc4ab9c92eb66c5b137e2
kaden-fugate <kkadenfugate@gmail.com>
2024-03-26 21:46:12 -0700

    updated template, created skeleton code for program

---
+++ compatibility-calc.py
@@ -0,0 +1,19 @@
+# will calculate the potting beds
+def calculateBeds():
+    pass
+
+# get csv from user, get potting bed dimensions
+def getUserInfo():
+    pass
+
+# will read the csv from the user
+def readCSV():
+    pass
+
+# do i even need to explain this function?
+def main():
+    pass
+
+# entry point for script
+if __name__ == "__main__":
+    main()
